8b708ebde5bd11f05753d0f4302bd5f12e92b27d
alextotalk <alextotalk@gmail.com>
2025-06-12 11:53:09 +0300

    Complete TypeScript advanced homework tasks 1-4: classes, access modifiers, interfaces, inheritance

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person came in successfully');
+    } else {
+      console.log('Door is closed, cannot come in');
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door opened successfully');
+    } else {
+      console.log('Wrong key, door remains closed');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
